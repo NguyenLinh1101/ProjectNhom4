e9f6c500432ee09ee24b952d4b74d09a39a79087
NguyenLinh1101 <ngoclinh2k51112@gmail.com>
2025-10-16 00:11:32 +0700

    bổ sung QL Đầu sách

--- ProjectNhom4/QL_DauSach.Designer.cs
+++ ProjectNhom4/QL_DauSach.Designer.cs
@@ -33,7 +33,18 @@
             this.lblDauSach = new System.Windows.Forms.Label();
             this.txtSearch = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2ContextMenuStrip1 = new Guna.UI2.WinForms.Guna2ContextMenuStrip();
+            this.grbTTDS = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.grbTTDS.SuspendLayout();
             this.SuspendLayout();
             // 
             // errorProvider1
@@ -92,10 +103,110 @@
             this.guna2ContextMenuStrip1.RenderStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
             this.guna2ContextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
+            // grbTTDS
+            // 
+            this.grbTTDS.Controls.Add(this.label8);
+            this.grbTTDS.Controls.Add(this.label7);
+            this.grbTTDS.Controls.Add(this.label6);
+            this.grbTTDS.Controls.Add(this.label5);
+            this.grbTTDS.Controls.Add(this.label4);
+            this.grbTTDS.Controls.Add(this.label3);
+            this.grbTTDS.Controls.Add(this.label2);
+            this.grbTTDS.Controls.Add(this.label1);
+            this.grbTTDS.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.grbTTDS.ForeColor = System.Drawing.Color.Black;
+            this.grbTTDS.Location = new System.Drawing.Point(57, 229);
+            this.grbTTDS.Name = "grbTTDS";
+            this.grbTTDS.Size = new System.Drawing.Size(1927, 330);
+            this.grbTTDS.TabIndex = 7;
+            this.grbTTDS.TabStop = false;
+            this.grbTTDS.Text = "Thông tin đầu sách";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(182, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mã đầu sách";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(182, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tên đầu sách";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(182, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Năm XB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(182, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Giá bìa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(632, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 28);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Số trang";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(1213, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 28);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Loại sách";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(1213, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 28);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Chủ đề";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(1213, 194);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 28);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Tác giả";
+            // 
             // QL_DauSach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.grbTTDS);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.lblDauSach);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
@@ -103,6 +214,8 @@
             this.Name = "QL_DauSach";
             this.Size = new System.Drawing.Size(2035, 1277);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.grbTTDS.ResumeLayout(false);
+            this.grbTTDS.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,5 +227,15 @@
         private System.Windows.Forms.Label lblDauSach;
         private Guna.UI2.WinForms.Guna2TextBox txtSearch;
         private Guna.UI2.WinForms.Guna2ContextMenuStrip guna2ContextMenuStrip1;
+        private System.Windows.Forms.GroupBox grbTTDS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
     }
 }
