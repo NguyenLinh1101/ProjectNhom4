cf76ba6ac2b106b4bf5597a7085949f580b93f7e
NguyenLinh1101 <ngoclinh2k51112@gmail.com>
2025-10-15 10:27:18 +0700

    Thay đổi form 1

--- ProjectNhom4/Form1.cs
+++ ProjectNhom4/Form1.cs
@@ -16,5 +16,10 @@ namespace ProjectNhom4
         {
             InitializeComponent();
         }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
