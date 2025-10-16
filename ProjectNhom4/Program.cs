c1d39edbcdb8a0602c454e309f2318c8c05b7f04
NguyenLinh1101 <ngoclinh2k51112@gmail.com>
2025-10-16 22:43:16 +0700

    thêm

--- ProjectNhom4/Program.cs
+++ ProjectNhom4/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectNhom4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ChiTietDauSach());
+            Application.Run(new frmLoaiSach());
         }
     }
 }
