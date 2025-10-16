aca318164175eb0387e7cfb22b3bc0a1be0e8bff
LAN NHI <LAN NHI@LANNHI>
2025-10-16 09:55:49 +0700

    Cập nhật quản lý cuốn sách

---
+++ ProjectNhom4/QLcuonsach.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectNhom4
+{
+    public partial class QLcuonsach : Form
+    {
+        public QLcuonsach()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
