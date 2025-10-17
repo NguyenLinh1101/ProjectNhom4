a8692cb3d5d52a8b9ae7e2ef764f3601a195bf3a
NguyenLinh1101 <ngoclinh2k51112@gmail.com>
2025-10-17 10:21:22 +0700

    đổi màu 1

--- ProjectNhom4/Properties/Resources.Designer.cs
+++ ProjectNhom4/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace ProjectNhom4.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap down_chevron {
+            get {
+                object obj = ResourceManager.GetObject("down-chevron", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap search {
             get {
                 object obj = ResourceManager.GetObject("search", resourceCulture);
--- ProjectNhom4/QLSach_Ribbon.Designer.cs
+++ ProjectNhom4/QLSach_Ribbon.Designer.cs
@@ -30,17 +30,17 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panelQLSach = new System.Windows.Forms.Panel();
-            this.btnDanhMuc = new Guna.UI2.WinForms.Guna2Button();
             this.btnCuonSach = new Guna.UI2.WinForms.Guna2Button();
             this.btnDauSach = new Guna.UI2.WinForms.Guna2Button();
             this.panelQLSach_contain = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnDanhMuc = new Guna.UI2.WinForms.Guna2Button();
             this.panelQLSach.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelQLSach
             // 
-            this.panelQLSach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(76)))), ((int)(((byte)(170)))));
+            this.panelQLSach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.panelQLSach.Controls.Add(this.btnDanhMuc);
             this.panelQLSach.Controls.Add(this.btnCuonSach);
             this.panelQLSach.Controls.Add(this.btnDauSach);
@@ -52,40 +52,16 @@
             this.panelQLSach.Size = new System.Drawing.Size(2035, 90);
             this.panelQLSach.TabIndex = 0;
             // 
-            // btnDanhMuc
-            // 
-            this.btnDanhMuc.BackColor = System.Drawing.SystemColors.Control;
-            this.btnDanhMuc.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
-            this.btnDanhMuc.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 5);
-            this.btnDanhMuc.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btnDanhMuc.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btnDanhMuc.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btnDanhMuc.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btnDanhMuc.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(76)))), ((int)(((byte)(170)))));
-            this.btnDanhMuc.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnDanhMuc.ForeColor = System.Drawing.Color.White;
-            this.btnDanhMuc.Image = global::ProjectNhom4.Properties.Resources.down_arrow;
-            this.btnDanhMuc.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.btnDanhMuc.ImageOffset = new System.Drawing.Point(10, 0);
-            this.btnDanhMuc.ImageSize = new System.Drawing.Size(25, 25);
-            this.btnDanhMuc.Location = new System.Drawing.Point(1078, 0);
-            this.btnDanhMuc.MaximumSize = new System.Drawing.Size(247, 90);
-            this.btnDanhMuc.Name = "btnDanhMuc";
-            this.btnDanhMuc.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(76)))), ((int)(((byte)(170)))));
-            this.btnDanhMuc.Size = new System.Drawing.Size(247, 90);
-            this.btnDanhMuc.TabIndex = 1;
-            this.btnDanhMuc.Text = "Danh mục";
-            // 
             // btnCuonSach
             // 
-            this.btnCuonSach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(76)))), ((int)(((byte)(170)))));
+            this.btnCuonSach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.btnCuonSach.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             this.btnCuonSach.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 5);
             this.btnCuonSach.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnCuonSach.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnCuonSach.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.btnCuonSach.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btnCuonSach.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(76)))), ((int)(((byte)(170)))));
+            this.btnCuonSach.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.btnCuonSach.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnCuonSach.ForeColor = System.Drawing.Color.White;
             this.btnCuonSach.Location = new System.Drawing.Point(674, 0);
@@ -103,7 +79,7 @@
             this.btnDauSach.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnDauSach.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.btnDauSach.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btnDauSach.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(76)))), ((int)(((byte)(170)))));
+            this.btnDauSach.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.btnDauSach.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnDauSach.ForeColor = System.Drawing.Color.White;
             this.btnDauSach.Location = new System.Drawing.Point(306, 0);
@@ -124,6 +100,30 @@
             // timer1
             // 
             this.timer1.Interval = 15;
+            // 
+            // btnDanhMuc
+            // 
+            this.btnDanhMuc.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDanhMuc.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnDanhMuc.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.btnDanhMuc.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDanhMuc.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnDanhMuc.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnDanhMuc.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnDanhMuc.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.btnDanhMuc.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDanhMuc.ForeColor = System.Drawing.Color.White;
+            this.btnDanhMuc.Image = global::ProjectNhom4.Properties.Resources.down_chevron;
+            this.btnDanhMuc.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.btnDanhMuc.ImageOffset = new System.Drawing.Point(10, 0);
+            this.btnDanhMuc.ImageSize = new System.Drawing.Size(25, 25);
+            this.btnDanhMuc.Location = new System.Drawing.Point(1078, 0);
+            this.btnDanhMuc.MaximumSize = new System.Drawing.Size(247, 90);
+            this.btnDanhMuc.Name = "btnDanhMuc";
+            this.btnDanhMuc.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.btnDanhMuc.Size = new System.Drawing.Size(247, 90);
+            this.btnDanhMuc.TabIndex = 1;
+            this.btnDanhMuc.Text = "Danh mục";
             // 
             // QLSach_Ribbon
             // 
